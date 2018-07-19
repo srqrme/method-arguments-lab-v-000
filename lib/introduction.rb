2329732dbf20aeb9498a1d96ee05e09fe0f62f05
Sharmaine Requerme <srqrme@gmail.com>
2018-07-19 17:14:55 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,3 +7,5 @@ introduction("Sharmaine")
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+
+introduction_with_language("Sharmaine", "Ruby")
